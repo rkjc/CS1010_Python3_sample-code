4a21598ee4254bf3086e084853f001a8a6649e94
rkjc <richardkjcross@gmail.com>
2021-03-17 09:29:23 -0700

    quick code update

--- 7-Lists_and_Collections/Lists-Collections-Demo/List-One.py
+++ 7-Lists_and_Collections/Lists-Collections-Demo/List-One.py
@@ -1,5 +1,13 @@
 print("------------------------\nList-One.py\n------------------------")
 
+myvar = ['happy', 'sad', 'orange','green', 'california']
+
+print( myvar )
+
+bob = 4
+
+print(  myvar[bob]   )
 
 
 
+print("------------------------")
