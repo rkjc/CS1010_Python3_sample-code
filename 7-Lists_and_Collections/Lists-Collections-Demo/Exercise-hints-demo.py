4c35289c4aaf2020716c1ec043f585ff76bb95dc
rkjc <richardkjcross@gmail.com>
2021-03-19 12:29:15 -0700

    quick code update

---
+++ 7-Lists_and_Collections/Lists-Collections-Demo/Exercise-hints-demo.py
@@ -0,0 +1,24 @@
+
+
+mystr = "happy joy sunshine"
+c = len( mystr )
+print(c)
+
+
+myvar1 = "A Red Fox"
+myvar2 = "chased a Blue mouse"
+myvar3 = "across a Green meadow"
+
+
+#poisoned!
+c = len(myvar3)
+
+print(c)
+
+
+
+mystr = "42"
+print( mystr.isnumeric() )
+
+
+
