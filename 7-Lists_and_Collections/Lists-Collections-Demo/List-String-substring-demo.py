34f2a2cf98d336085171c495b4b5bf1390cbc3be
rkjc <richardkjcross@gmail.com>
2021-03-19 12:11:36 -0700

    quick code update

---
+++ 7-Lists_and_Collections/Lists-Collections-Demo/List-String-substring-demo.py
@@ -0,0 +1,18 @@
+#subsets spans  subarrays sublists substrings
+
+numarr = [45, 345, 98, 1001, 42, 121, 7, 2, 0, 1970]
+print(numarr)
+
+print(numarr[ 5 ])
+print(numarr[ 2:7 ])
+
+
+luke = "The quick red fox crushed the chrome window"
+
+print(luke[10])
+print(luke[10:19] )
+print(luke[ :17])
+print(luke[ 26: ])
+
+print(luke.count('c'))
+
