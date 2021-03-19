9a410f4ae0c9c2f04997c0e72172628a2cdce0b9
rkjc <richardkjcross@gmail.com>
2021-03-19 12:57:39 -0700

    quick code update

---
+++ 7-Lists_and_Collections/Lists-Collections-Demo/Ex2-List-lab.py
@@ -0,0 +1,31 @@
+# richard cross
+# Ex2
+
+#Ex2-a
+my_num_list = [3, 6, 4, 83, 2, 7, 2, 17, 4, 9, 0, 44, 2]
+print("----------------")
+
+#Ex2-b - Print out the 4th element in the List (index of 3).
+
+print(my_num_list[3])
+print("----------------")
+# Ex2-C.  Append a new value "8" to the list, and print out the list.
+
+my_num_list.append(8)
+print(my_num_list)
+print("----------------")
+
+# Ex2-D.  Insert "33" after the "83" in the list, and print out the list.
+
+# Ex2-E.  Print out how many '2's are in the list.
+
+# Ex2-F.  Sort the list in ascending order and print out the list.
+
+# Ex2-G.  Print out how many elements are in the list.
+
+# Ex2-H.  Remove and print the last 4 elements of the list.
+
+# Ex2-I.  Print out how many elements are in the list now.
+
+# Ex2-J.  Use a while loop and the index function to print out each element in the array.
+#     (*hint:  use the len() function to control at what count the While loop will stop)
--- 7-Lists_and_Collections/Lists-Collections-Demo/Exercise-hints-demo.py
+++ 7-Lists_and_Collections/Lists-Collections-Demo/Exercise-hints-demo.py
@@ -16,9 +16,12 @@ c = len(myvar3)
 print(c)
 
 
-
+#Ex1-i
 mystr = "42"
 print( mystr.isnumeric() )
 
+
+my_num_list = [3, 6, 4, 83, 2, 7, 2, 17, 4, 9, 0, 44, 2]
+print( my_num_list )
 
 
