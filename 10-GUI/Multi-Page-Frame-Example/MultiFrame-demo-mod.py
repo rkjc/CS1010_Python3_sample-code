22ca98749ef6b329bf90d34ec1fbe65602a51ead
rkjc <richardkjcross@gmail.com>
2021-04-30 11:43:27 -0700

    quick code update

---
+++ 10-GUI/Multi-Page-Frame-Example/MultiFrame-demo-mod.py
@@ -0,0 +1,77 @@
+#https://stackoverflow.com/questions/50422735/tkinter-resize-frame-and-contents-with-main-window
+
+from tkinter import *
+from tkinter import scrolledtext
+
+mw = Tk()
+mw.title("Multi-window demo code - 1")
+mw.geometry("600x500")
+
+mw.columnconfigure(0, weight=1)
+mw.rowconfigure(1, weight=1)
+
+def showRed():
+    redFrame.tkraise()
+
+def showGreen():
+    greenFrame.tkraise()
+
+
+def showBlue():
+    blueFrame.tkraise()
+
+# Parent widget for the buttons
+buttons_frame = Frame(mw, bg="cyan", highlightbackground="black", highlightthickness=1)
+
+buttons_frame.grid(row=0, column=0, columnspan=3, sticky=N+W+E)
+
+btn_1 = Button(buttons_frame, text='red', width='5', command=showRed)
+btn_1.grid(row=0, column=0, padx=(10), pady=10)
+
+btn_2 = Button(buttons_frame, text='green', width='5', command=showGreen)
+btn_2.grid(row=0, column=1, padx=(10), pady=10)
+
+btn_3 = Button(buttons_frame, text='blue', width='5', command=showBlue)
+btn_3.grid(row=0, column=2, padx=(10), pady=10)
+
+# bottom Frame ----------------------------------------------------
+bottom = Frame(mw, bg="yellow", padx=5, pady=5)
+bottom.grid_propagate(False)
+bottom.grid(row=1, column=0, padx=10, pady=10, sticky=N+W+S+E)
+
+bottom.columnconfigure(0, weight=1)
+bottom.rowconfigure(0, weight=1)
+
+# red Frame ----------------------------------------------------
+redFrame = Frame(bottom, bg="red", padx=5, pady=5, width=300, height=300)
+redFrame.grid_propagate(False)
+redFrame.grid(row=0, column=0)
+#redFrame.grid(row=0, column=0, padx=(10,60), pady=(10,60))
+redFrame.columnconfigure(0, weight=1)
+redFrame.rowconfigure(0, weight=1)
+
+redLabel_1 = Label(redFrame, text="this is the Red Frame")
+redLabel_1.grid(column=0, row=0)
+
+# green Frame ----------------------------------------------------
+greenFrame = Frame(bottom, bg="green", padx=5, pady=5, width=300, height=300)
+greenFrame.grid_propagate(False)
+greenFrame.grid(row=0, column=0)
+greenFrame.columnconfigure(0, weight=1)
+greenFrame.rowconfigure(0, weight=1)
+
+greenLabel_1 = Label(greenFrame, text="this is the Green Frame")
+greenLabel_1.grid(column=0, row=0)
+
+# blue Frame ----------------------------------------------------
+blueFrame = Frame(bottom, bg="blue", padx=5, pady=5, width=300, height=300)
+blueFrame.grid_propagate(False)
+blueFrame.grid(row=0, column=0)
+blueFrame.columnconfigure(0, weight=1)
+blueFrame.rowconfigure(0, weight=1)
+
+blueLabel_1 = Label(blueFrame, text="this is the Blue Frame")
+blueLabel_1.grid(column=0, row=0)
+
+
+mainloop()
--- 10-GUI/Multi-Page-Frame-Example/multiPage-4_S20.py
+++ 10-GUI/Multi-Page-Frame-Example/multiPage-4_S20.py
@@ -1,14 +1,14 @@
 #https://stackoverflow.com/questions/50422735/tkinter-resize-frame-and-contents-with-main-window
 
-from tkinter import *
+import tkinter as tk
 from tkinter import scrolledtext
 
-mw = Tk()
-mw.title("Multi-window demo code - 4")
-mw.geometry("600x500")
+mainwindow = tk.Tk()
+mainwindow.title("Multi-window demo code - 3")
+mainwindow.geometry("600x500")
 
-mw.columnconfigure(0, weight=1)
-mw.rowconfigure(1, weight=1)
+mainwindow.columnconfigure(0, weight=1)
+mainwindow.rowconfigure(1, weight=1)
 
 def showRed():
     redFrame.tkraise()
@@ -20,65 +20,69 @@ def showGreen():
 def showBlue():
     blueFrame.tkraise()
 
+# Parent widget for the buttons
+top_frame = tk.Frame(mainwindow, bg="cyan", highlightbackground="black", highlightthickness=1)
+
+top_frame.grid(row=0, column=0, columnspan=3, sticky=tk.N+tk.W+tk.E)
+
+btn_r1 = tk.Button(top_frame, text='red', width='5', command=showRed)
+btn_r1.grid(row=0, column=0, padx=(10), pady=10)
+
+btn_g1 = tk.Button(top_frame, text='green', width='5', command=showGreen)
+btn_g1.grid(row=0, column=1, padx=(10), pady=10)
+
+btn_b1 = tk.Button(top_frame, text='blue', width='5', command=showBlue)
+btn_b1.grid(row=0, column=2, padx=(10), pady=10)
 
 # bottom Frame ----------------------------------------------------
-bottom = Frame(mw, bg="yellow")
-bottom.grid_propagate(False)
-bottom.grid(row=1, column=0, sticky=N+W+S+E)
+bottom_frame = tk.Frame(mainwindow, bg="yellow", padx=5, pady=5)
+bottom_frame.grid_propagate(False)
+bottom_frame.grid(row=1, column=0, padx=10, pady=10, sticky=(tk.NSEW))
 
-bottom.columnconfigure(0, weight=1)
-bottom.rowconfigure(0, weight=1)
+bottom_frame.columnconfigure(0, weight=1)
+bottom_frame.rowconfigure(0, weight=1)
 
 # red Frame ----------------------------------------------------
-redFrame = Frame(bottom, bg="red")
-redFrame.grid_propagate(False)
-redFrame.grid(row=0, column=0, sticky=N+W+S+E)
-redFrame.columnconfigure(0, weight=1)
-redFrame.rowconfigure(0, weight=1)
+redFrame = tk.Frame(bottom_frame, bg="red", padx=5, pady=5)
+redFrame.place(x=0, y=0, relwidth=1.0, relheight=1.0)
 
-redLabel_1 = Label(redFrame, text="this is the Red Frame")
-redLabel_1.grid(column=0, row=0)
+redLabel_1 = tk.Label(redFrame, text="this is the Red Frame")
+redLabel_1.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-btn_b2 = Button(redFrame, fg="blue", text='blue', width='5', command=showBlue)
-btn_b2.grid(row=0, column=0, padx=10, pady=10, sticky=N+W)
+btn_b2 = tk.Button(redFrame, fg="blue", text='blue', width='5', command=showBlue)
+btn_b2.place(x=0, y=0)
 
-btn_g2 = Button(redFrame, fg="green", text='green', width='5', command=showGreen)
-btn_g2.grid(row=0, column=0, padx=10, pady=10, sticky=N+E)
+btn_g2 = tk.Button(redFrame, fg="green", text='green', width='5', command=showGreen)
+btn_g2.place(relx=1.0, y=0, anchor=(tk.NE))
 
 
 # green Frame ----------------------------------------------------
-greenFrame = Frame(bottom, bg="green")
-greenFrame.grid_propagate(False)
-greenFrame.grid(row=0, column=0, sticky=N+W+S+E)
-greenFrame.columnconfigure(0, weight=1)
-greenFrame.rowconfigure(0, weight=1)
+greenFrame = tk.Frame(bottom_frame, bg="green", padx=5, pady=5)
+greenFrame.place(x=0, y=0, relwidth=1.0, relheight=1.0)
 
-greenLabel_1 = Label(greenFrame, text="this is the Green Frame")
-greenLabel_1.grid(column=0, row=0)
+greenLabel_1 = tk.Label(greenFrame, text="this is the Green Frame")
+greenLabel_1.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-btn_r3 = Button(greenFrame, fg="red", text='red', width='5', command=showRed)
-btn_r3.grid(row=0, column=0, padx=10, pady=10, sticky=N+W)
+btn_r3 = tk.Button(greenFrame, fg="red", text='red', width='5', command=showRed)
+btn_r3.place(x=0, y=0)
 
-btn_b3 = Button(greenFrame, fg="blue", text='blue', width='5', command=showBlue)
-btn_b3.grid(row=0, column=0, padx=10, pady=10, sticky=N+E)
+btn_b3 = tk.Button(greenFrame, fg="blue", text='blue', width='5', command=showBlue)
+btn_b3.place(relx=1.0, y=0, anchor=(tk.NE))
 
 
 
 # blue Frame ----------------------------------------------------
-blueFrame = Frame(bottom, bg="blue")
-blueFrame.grid_propagate(False)
-blueFrame.grid(row=0, column=0, sticky=N+W+S+E)
-blueFrame.columnconfigure(0, weight=1)
-blueFrame.rowconfigure(0, weight=1)
+blueFrame = tk.Frame(bottom_frame, bg="blue", padx=5, pady=5)
+blueFrame.place(x=0, y=0, relwidth=1.0, relheight=1.0)
 
-blueLabel_1 = Label(blueFrame, text="this is the Blue Frame")
-blueLabel_1.grid(column=0, row=0)
+blueLabel_1 = tk.Label(blueFrame, text="this is the Blue Frame")
+blueLabel_1.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-btn_g4 = Button(blueFrame, fg="green", text='green', width='5', command=showGreen)
-btn_g4.grid(row=0, column=0, padx=10, pady=10, sticky=N+W)
+btn_g4 = tk.Button(blueFrame, fg="green", text='green', width='5', command=showGreen)
+btn_g4.place(x=0, y=0)
 
-btn_r4 = Button(blueFrame, fg="red", text='red', width='5', command=showRed)
-btn_r4.grid(row=0, column=0, padx=10, pady=10, sticky=N+E)
+btn_r4 = tk.Button(blueFrame, fg="red", text='red', width='5', command=showRed)
+btn_r4.place(relx=1.0, y=0, anchor=(tk.NE))
 
 
-mainloop()
+mainwindow.mainloop()
